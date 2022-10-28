eb5a0aef3272b0a0e18974ae5352c771d8d75d57
Zens <zens@Zenss-MacBook-Pro.local>
2022-10-28 13:10:50 +0700

    Ehencement - remove file posts to root

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
-import Date from "../../components/date";
 import Head from "next/head";
+import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({ postData }) {
@@ -22,15 +22,14 @@ export default function Post({ postData }) {
 }
 
 export async function getStaticPaths() {
-	// Return a list of possible value for id
 	const paths = getAllPostIds();
 	return {
 		paths,
 		fallback: false,
 	};
 }
+
 export async function getStaticProps({ params }) {
-	// Fetch necessary data for the blog post using params.id
 	const postData = await getPostData(params.id);
 	return {
 		props: {
